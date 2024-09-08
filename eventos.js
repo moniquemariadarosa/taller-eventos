47b8284d44e7b2435f48b20a5b06ef596fbd938f
moniquemariadarosa <moniqueeei53@gmail.com>
2024-09-07 23:30:54 -0300

    Update eventos.js

--- eventos.js
+++ eventos.js
@@ -1,3 +1,8 @@
 document.getElementById("Div").addEventListener("click", function() {
     alert("Hola! Soy el div");
 });
+
+document.getElementById("miBoton").addEventListener("click", function(event) {
+    alert('Hola!');
+    event.stopPropagation(); 
+});
